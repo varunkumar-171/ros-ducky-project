67e09c034a93e7cbb193ada9d03f5edd45b612d8
Varun Kumar Siva <varunkumar.siva@gmail.com>
2024-05-17 15:51:22 +0200

    duckiebot

---
+++ assets/vnc/usr/bin/launch-data-collection.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /code/catkin_ws/src/object-detection/packages/utils/data_collection.py
---
+++ assets/vnc/usr/bin/launch-nn-model-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec rqt_image_view /${VEHICLE_NAME}/object_detection_node/image/compressed
